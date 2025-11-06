721c8074dceca0c22c765f33c6df6bca3818dfcb
David-Ludtke <36012@superior.k12.wi.us>
2025-11-06 08:10:09 -0600

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+package src; //main is included in the path src so we say the package is src I did not go over packages so
+//I make this as a example
+
+public class Main {
+  private Main() {} //we force only calling main and not it's constructor.
+  public static void main(String[] args) { //main function that takes an String array (command line arguments)
+    var startprogram = new program.Start.create(args); //create an instance of start with a factory constructer
+    startprogram.start(); //call start to start the actual program
+  }
+}
