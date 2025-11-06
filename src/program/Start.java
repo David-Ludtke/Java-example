0ad01a9114da36474d06d40d371b35f1701f95ab
David-Ludtke <36012@superior.k12.wi.us>
2025-11-06 10:26:06 -0600

    fixed all syntax errors in Start.java

--- src/program/Start.java
+++ src/program/Start.java
@@ -1,6 +1,9 @@
 package src/program; //reflects file location
 import java.util.Optional; //pull the Optional class into the golbal scope
 import java.util.Scanner; //a wrapper class to make interacting with input stream easier
+import java.text.CharacterIterator; //iterator class
+import java.text.StringCharacterIterator; //class to make string into char iterator
+//to iterator method
 
 public class Start {
   private String[] CLArgs; //Command Line Arguments
@@ -22,22 +25,23 @@ public class Start {
       }
       
       var input = maybeInput.get(); //now safe to get input because we checked for it not being present
-      var maybeFinalCalc = this.calculate(new StringCharactorIterator(input)); //make a charactorIterator to pass
-      if (!maybeFinalCalc.isPresent() { //checks if not present
-        System.out.println("not a valid expresion"); //tells user expresion was not valid
+      var maybeFinalCalc = this.calculate(new StringCharacterIterator(input)); //make a charactorIterator to pass
+      if (!maybeFinalCalc.isPresent()) { //checks if not present
+        System.out.println("\"" + input  + "\"" + " is not a valid expresion"); //tells user expresion was not valid
         continue; //pass to next iteratation
       }
-      System.out.println(input.get()) //print the result if seccesfull (bad spelling)
+      System.out.println(maybeFinalCalc.get()); //print the result if seccesfull (bad spelling)
     }
   }
   private Optional<String> getInput() {
-    Scanner inputstream = new Scanner(System.in); //pass in input stream to the scanner to wrap it around
-    if (!inputstream.hasNextLine()) { //checks if theres no line to read
+    Scanner inputStream = new Scanner(System.in); //pass in input stream to the scanner to wrap it around
+    if (!inputStream.hasNextLine()) { //checks if theres no line to read
       return Optional.empty(); //return an empty option so input reading failed
     }
-    return Scanner.nextLine(); //theres never an exception now and the caller must handle case with no string
+    return Optional.of(inputStream.nextLine()); //theres never an exception now and the caller must handle 
+    //case with no string
   }
-  private Optional<String> calculate(CharactorIterator expr) { //take an iterator to allow recursiveness
+  private Optional<String> calculate(CharacterIterator expr) { //take an iterator to allow recursiveness
     return Optional.empty(); //Todo!();
   }
 }
